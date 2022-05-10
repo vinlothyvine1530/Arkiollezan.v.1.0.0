233e68c8d675648700d12aaf97d24449acb0175a
vinlothyvine1530 <95204153+vinlothyvine1530@users.noreply.github.com>
2022-05-11 03:51:53 +0800

    tanan

---
+++ footer.php
@@ -0,0 +1,57 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p> A Clinic Management System (CMIS) refers to a system designed to manage healthcare data. This includes systems that collect, store, manage and transmit a patient’s electronic medical record (EMR), a Clinic’s operational management or a system supporting healthcare policy decisions. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>SOCIAL LINKS</h6>
+            <p><a href="https://#/">Official Website</a></p>
+            <p><a href="https://www.facebook.com/ctunagaclinic">Facebook</a></p>
+            <p><a href="https://www.youtube.com/">Youtube</a></p>
+            <p><a href="https://www.twitter.com/">Twitter</a></p>
+            <p><a href="https://www.linkedin.com/">LinkedIn</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© 2022 Clinical Monitoring and Inventory System | Developed By: <strong><span></span><a href="#" style="color: white">ARKIOLLEZAN</a></span></strong>  </p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
